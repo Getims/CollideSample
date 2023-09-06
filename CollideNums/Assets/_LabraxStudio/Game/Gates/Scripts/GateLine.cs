a8b53832fd0cbc8e2277bf74b0edbcc4555d4ef4
GeTimS <getims@yandex.ru>
2023-09-06 11:52:59 +0300

    Fix gates, saw particles and tutorial #861na4c95

--- CollideNums/Assets/_LabraxStudio/Game/Gates/Scripts/GateLine.cs
+++ CollideNums/Assets/_LabraxStudio/Game/Gates/Scripts/GateLine.cs
@@ -1,15 +1,34 @@
 using System;
+using System.Collections.Generic;
+using LabraxStudio.Meta.GameField;
 using UnityEngine;
 
 namespace LabraxStudio.Game.Gates.Visual
 {
     [Serializable]
-    public class GateLine
+    public class GateLine : MonoBehaviour
     {
         // MEMBERS: -------------------------------------------------------------------------------
 
         [SerializeField]
         private Transform _gateContainer;
+
+
+        [SerializeField]
+        private SpriteRenderer _gateHorizontal;
+
+        [SerializeField]
+        private SpriteRenderer _gateHorizontalBottom1;
+
+        [SerializeField]
+        private SpriteRenderer _gateHorizontalBottom2;
+
+        [SerializeField]
+        private SpriteRenderer _gateHorizontalBottom3;
+
+        [SerializeField]
+        private SpriteRenderer _gateHorizontalBottom4;
+
 
         [SerializeField]
         private SpriteRenderer _gateVertical;
@@ -24,22 +43,17 @@ namespace LabraxStudio.Game.Gates.Visual
         private SpriteRenderer _gateVerticalCut;
 
         [SerializeField]
-        private SpriteRenderer _gateHorizontalBottom1;
+        private List<SpriteRenderer> _highlights;
 
         [SerializeField]
-        private SpriteRenderer _gateHorizontalBottom2;
+        private List<SpriteRenderer> _horizontalOpenLine = new List<SpriteRenderer>();
 
         [SerializeField]
-        private SpriteRenderer _gateHorizontalBottom3;
-
-        [SerializeField]
-        private SpriteRenderer _gateHorizontalBottom4;
-
-        [SerializeField]
-        private SpriteRenderer _gateHorizontal;
+        private List<SpriteRenderer> _verticalOpenLine = new List<SpriteRenderer>();
 
         // FIELDS: -------------------------------------------------------------------
 
+        private bool _useColor;
         private Color _openedGateColor;
         private Color _closedGateColor;
         private SpriteRenderer _currentGate;
@@ -50,14 +64,27 @@ namespace LabraxStudio.Game.Gates.Visual
 
         public void SetState(bool isLocked)
         {
-            var color = isLocked ? _closedGateColor : _openedGateColor;
-            _currentGate.color = color;
+            if (_useColor)
+            {
+                var color = isLocked ? _closedGateColor : _openedGateColor;
+                _currentGate.color = color;
+            }
+            else
+            {
+                _currentGate.enabled = isLocked;
+                SetOpenLineState(!isLocked);
+            }
         }
 
-        public void Setup(Color UnlockedColor, Color LockedColor, Direction direction, int gateType)
+        public void Setup(GatesSprites gatesSprites, Direction direction, int gateType)
         {
-            _openedGateColor = UnlockedColor;
-            _closedGateColor = LockedColor;
+            _useColor = gatesSprites.UseColor;
+            _openedGateColor = gatesSprites.UnlockedColor;
+            _closedGateColor = gatesSprites.LockedColor;
+
+            SetupHighlights(_useColor);
+            SetupOpenSprites(_useColor, gatesSprites.HorizontalOpenLine, gatesSprites.VerticalOpenLine);
+            SetSprites(gatesSprites);
 
             switch (direction)
             {
@@ -91,7 +118,53 @@ namespace LabraxStudio.Game.Gates.Visual
             }
 
             _currentGate.gameObject.SetActive(true);
-            _currentGate.color = _openedGateColor;
+            if (_useColor)
+                _currentGate.color = _openedGateColor;
+        }
+
+        private void SetSprites(GatesSprites gatesSprites)
+        {
+            _gateHorizontal.sprite = gatesSprites.GateHorizontal;
+            _gateHorizontalBottom1.sprite = gatesSprites.GateHorizontalBottom1;
+            _gateHorizontalBottom2.sprite = gatesSprites.GateHorizontalBottom2;
+            _gateHorizontalBottom3.sprite = gatesSprites.GateHorizontalBottom3;
+            _gateHorizontalBottom4.sprite = gatesSprites.GateHorizontalBottom4;
+            _gateVertical.sprite = gatesSprites.GateVertical;
+            _gateVerticalTop.sprite = gatesSprites.GateVerticalTop;
+            _gateVerticalBottom.sprite = gatesSprites.GateVerticalBottom;
+            _gateVerticalCut.sprite = gatesSprites.GateVerticalCut;
+        }
+
+        private void SetupOpenSprites(bool useColor, Sprite horizontalOpenLineSprite, Sprite verticalOpenLineSprite)
+        {
+            foreach (var openLine in _horizontalOpenLine)
+            {
+                openLine.sprite = horizontalOpenLineSprite;
+                openLine.enabled = false;
+                openLine.gameObject.SetActive(!useColor);
+            }
+
+
+            foreach (var openLine in _verticalOpenLine)
+            {
+                openLine.sprite = verticalOpenLineSprite;
+                openLine.enabled = false;
+                openLine.gameObject.SetActive(!useColor);
+            }
+        }
+
+        private void SetupHighlights(bool useColor)
+        {
+            foreach (var highlight in _highlights)
+                highlight.enabled = _useColor;
+        }
+
+        private void SetOpenLineState(bool enabled)
+        {
+            foreach (var openLine in _horizontalOpenLine)
+                openLine.enabled = enabled;
+            foreach (var openLine in _verticalOpenLine)
+                openLine.enabled = enabled;
         }
     }
 }
--- CollideNums/Assets/_LabraxStudio/Meta/Scripts/Game Field/GatesSprites.cs
+++ CollideNums/Assets/_LabraxStudio/Meta/Scripts/Game Field/GatesSprites.cs
@@ -19,20 +19,65 @@ namespace LabraxStudio.Meta.GameField
 
         [Title("Gate line")]
         [SerializeField]
+        private bool _useColor = true;
+
+        [SerializeField]
+        [ShowIf(nameof(_useColor))]
         private Color _unlockedColor = Color.white;
 
         [SerializeField]
+        [ShowIf(nameof(_useColor))]
         private Color _lockedColor = Color.black;
 
-        [Title("Numbers")] 
+        [SerializeField]
+        [HideIf(nameof(_useColor))]
+        private Sprite _horizontalOpenLine;
+        
+        [SerializeField]
+        [HideIf(nameof(_useColor))]
+        private Sprite _verticalOpenLine;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateHorizontal;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateHorizontalBottom1;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateHorizontalBottom2;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateHorizontalBottom3;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateHorizontalBottom4;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateVertical;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateVerticalTop;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateVerticalBottom;
+
+        [SerializeField, LabelWidth(175)]
+        private Sprite _gateVerticalCut;
+
+        [Title("Numbers")]
         [SerializeField]
         private Color _numberBaseColor = Color.white;
 
         [SerializeField]
         private Color _numberGlowColor = Color.white;
-        
+
         [SerializeField, LabelWidth(175)]
         private List<Sprite> _gatesNumbers = new List<Sprite>();
+
+        public GatesSprites(Sprite verticalOpenLine)
+        {
+            _verticalOpenLine = verticalOpenLine;
+        }
 
         // PROPERTIES: ----------------------------------------------------------------------------
 
@@ -49,5 +94,19 @@ namespace LabraxStudio.Meta.GameField
         public Color NumberBaseColor => _numberBaseColor;
 
         public Color NumberGlowColor => _numberGlowColor;
+        
+        public Sprite HorizontalOpenLine => _horizontalOpenLine;
+        public Sprite VerticalOpenLine => _verticalOpenLine;
+        public bool UseColor => _useColor;
+        public Sprite GateHorizontal => _gateHorizontal;
+        public Sprite GateHorizontalBottom1 => _gateHorizontalBottom1;
+        public Sprite GateHorizontalBottom2 => _gateHorizontalBottom2;
+        public Sprite GateHorizontalBottom3 => _gateHorizontalBottom3;
+        public Sprite GateHorizontalBottom4 => _gateHorizontalBottom4;
+        public Sprite GateVertical => _gateVertical;
+        public Sprite GateVerticalTop => _gateVerticalTop;
+        public Sprite GateVerticalBottom => _gateVerticalBottom;
+        public Sprite GateVerticalCut => _gateVerticalCut;
+
     }
 }
